2134f232783e9ba7702105727e43a7a215591c27
Adriano Orlando <adrianoorlando04@gmail.com>
2022-11-26 17:44:38 +1300

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,13 +1,13 @@
 $('body').on('click', '#ts-emma-toggle-entry', function(e) {
   e.preventDefault();
-  $(".ts-pop-up-container-screen").addClass("ts-pop-up-container-screen--active");
+  $(".ts-page").addClass("ts-page--active");
   $(".ts-pop-up-container-screen--emma").addClass("ts-pop-up-container-screen--emma--entry");
   $(".ts-pop-up-container-screen--emma").removeClass("ts-pop-up-container-screen--emma--exit");
 });
 
 $('body').on('click', '#ts-emma-toggle-exit', function(e) {
   e.preventDefault();
-  $(".ts-pop-up-container-screen").removeClass("ts-pop-up-container-screen--active");
+  $(".ts-page").removeClass("ts-page--active");
   $(".ts-pop-up-container-screen--emma").addClass("ts-pop-up-container-screen--emma--exit");
   $(".ts-pop-up-container-screen--emma").removeClass("ts-pop-up-container-screen--emma--entry");
 });
@@ -19,4 +19,5 @@ $('.ts-slider-container').slick({
   prevArrow: $(".prev"),
   nextArrow: $(".next"),
   vertical: true,
+  swipe: false,
 });
