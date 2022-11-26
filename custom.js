9ee9f1c1e075ffa50119c1b258b2e94b83263145
Adriano Orlando <adrianoorlando04@gmail.com>
2022-11-26 13:28:16 +1300

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,41 +1,21 @@
-$('body').on('click', '#ts-project-toggle', function(e) {
+$('body').on('click', '#ts-emma-toggle-entry', function(e) {
   e.preventDefault();
-  $(".ts-link--project").toggleClass("ts-link--project--show");
-  $(".ts-link--click--project").toggleClass("ts-link--click--show");
+  $(".ts-pop-up-container-screen").addClass("ts-pop-up-container-screen--active");
+  $(".ts-pop-up-container-screen--emma").addClass("ts-pop-up-container-screen--emma--entry");
+  $(".ts-pop-up-container-screen--emma").removeClass("ts-pop-up-container-screen--emma--exit");
 });
 
-$('body').on('click', '#ts-contact-toggle', function(e) {
+$('body').on('click', '#ts-emma-toggle-exit', function(e) {
   e.preventDefault();
-  $(".ts-link--contact").toggleClass("ts-link--contact--show");
-  $(".ts-link--click--contact").toggleClass("ts-link--click--show");
+  $(".ts-pop-up-container-screen").removeClass("ts-pop-up-container-screen--active");
+  $(".ts-pop-up-container-screen--emma").addClass("ts-pop-up-container-screen--emma--exit");
+  $(".ts-pop-up-container-screen--emma").removeClass("ts-pop-up-container-screen--emma--entry");
 });
 
-$('body').on('click', '#ts-emma-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-project-container--emma").toggleClass("ts-project-container--show");
-  $(".ts-link--emma").toggleClass("ts-link--click--show");
-});
+// slider
 
-$('body').on('click', '#ts-geoffery-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-project-container--geoffery").toggleClass("ts-project-container--show");
-  $(".ts-link--geoffery").toggleClass("ts-link--click--show");
-});
-
-$('body').on('click', '#ts-grace-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-project-container--grace").toggleClass("ts-project-container--show");
-  $(".ts-link--grace").toggleClass("ts-link--click--show");
-});
-
-$('body').on('click', '#ts-jing-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-project-container--jing").toggleClass("ts-project-container--show");
-  $(".ts-link--jing").toggleClass("ts-link--click--show");
-});
-
-$('body').on('click', '#ts-matt-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-project-container--matt").toggleClass("ts-project-container--show");
-  $(".ts-link--matt").toggleClass("ts-link--click--show");
+$('.ts-slider-container').slick({
+  infinite: true,
+  prevArrow: $(".prev"),
+  nextArrow: $(".next"),
 });
