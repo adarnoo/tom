fbd19d56d20447c80af93f71d923d2ac32287be7
Adriano Orlando <adrianoorlando04@gmail.com>
2022-12-23 12:44:35 +1300

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,41 +1,28 @@
-$('body').on('click', '#ts-emma-toggle', function(e) {
+$('body').on('click', '#ts-small-toggle', function(e) {
   e.preventDefault();
-  $(".ts-pop-up-container-screen--emma").toggleClass("ts-pop-up-container-screen--emma--show");
-  $(".ts-page").toggleClass("ts-page--active");
-  $(".ts-page-blur").toggleClass("ts-page-blur--active");
-  $(".ts-overflow").toggleClass("ts-overflow--active");
+  $(".ts-feed-inner-container").removeClass("ts-feed-inner-container--medium");
+  $(".ts-feed-inner-container").removeClass("ts-feed-inner-container--large");
+  $(".ts-grid-switch--large").removeClass("ts-grid-switch--active-large");
+  $(".ts-grid-switch--medium").removeClass("ts-grid-switch--active-medium");
+  $(".ts-grid-switch--small").addClass("ts-grid-switch--active-small");
 });
 
-$('body').on('click', '#ts-grace-toggle', function(e) {
+$('body').on('click', '#ts-medium-toggle', function(e) {
   e.preventDefault();
-  $(".ts-pop-up-container-screen--grace").toggleClass("ts-pop-up-container-screen--grace--show");
-  $(".ts-page").toggleClass("ts-page--active");
-  $(".ts-page-blur").toggleClass("ts-page-blur--active");
-  $(".ts-overflow").toggleClass("ts-overflow--active");
+  $(".ts-feed-inner-container").addClass("ts-feed-inner-container--medium");
+  $(".ts-feed-inner-container").removeClass("ts-feed-inner-container--large");
+  $(".ts-grid-switch--large").removeClass("ts-grid-switch--active-large");
+  $(".ts-grid-switch--medium").addClass("ts-grid-switch--active-medium");
+  $(".ts-grid-switch--small").removeClass("ts-grid-switch--active-small");
 });
 
-$('body').on('click', '#ts-jing-toggle', function(e) {
+$('body').on('click', '#ts-large-toggle', function(e) {
   e.preventDefault();
-  $(".ts-pop-up-container-screen--jing").toggleClass("ts-pop-up-container-screen--jing--show");
-  $(".ts-page").toggleClass("ts-page--active");
-  $(".ts-page-blur").toggleClass("ts-page-blur--active");
-  $(".ts-overflow").toggleClass("ts-overflow--active");
-});
-
-$('body').on('click', '#ts-matt-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-pop-up-container-screen--matt").toggleClass("ts-pop-up-container-screen--matt--show");
-  $(".ts-page").toggleClass("ts-page--active");
-  $(".ts-page-blur").toggleClass("ts-page-blur--active");
-  $(".ts-overflow").toggleClass("ts-overflow--active");
-});
-
-$('body').on('click', '#ts-video-toggle', function(e) {
-  e.preventDefault();
-  $(".ts-video-container-screen").toggleClass("ts-video-container-screen--show");
-  $(".ts-page").toggleClass("ts-page--active");
-  $(".ts-page-blur").toggleClass("ts-page-blur--active");
-  $(".ts-overflow").toggleClass("ts-overflow--active");
+  $(".ts-feed-inner-container").addClass("ts-feed-inner-container--large");
+  $(".ts-feed-inner-container").removeClass("ts-feed-inner-container--medium");
+  $(".ts-grid-switch--large").addClass("ts-grid-switch--active-large");
+  $(".ts-grid-switch--medium").removeClass("ts-grid-switch--active-medium");
+  $(".ts-grid-switch--small").removeClass("ts-grid-switch--active-small");
 });
 
 // Email hover
@@ -48,36 +35,6 @@ $('body').on('mouseover', '#ts-email-toggle', function(e) {
 $('body').on('mouseleave', '#ts-email-toggle', function(e) {
   e.preventDefault();
   $(".ts-email").removeClass("ts-email--hover");
-});
-
-// Slider
-
-$('.ts-slider-container-1').slick({
-  infinite: true,
-  prevArrow: $(".prev-1"),
-  nextArrow: $(".next-1"),
-  swipe: true,
-});
-
-$('.ts-slider-container-2').slick({
-  infinite: true,
-  prevArrow: $(".prev-2"),
-  nextArrow: $(".next-2"),
-  swipe: true,
-});
-
-$('.ts-slider-container-3').slick({
-  infinite: true,
-  prevArrow: $(".prev-3"),
-  nextArrow: $(".next-3"),
-  swipe: true,
-});
-
-$('.ts-slider-container-4').slick({
-  infinite: true,
-  prevArrow: $(".prev-4"),
-  nextArrow: $(".next-4"),
-  swipe: true,
 });
 
 // Video player
